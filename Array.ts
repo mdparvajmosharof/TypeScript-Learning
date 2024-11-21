2f98ad08d88afd192447b5e80d16356d0cbce05f
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2024-11-21 18:53:55 +0600

    Using Array in ts

---
+++ Array.ts
@@ -0,0 +1,19 @@
+let Names: string[] = [];
+let Marks: Array<number> =[]
+
+Names.push("gg");
+Marks.push(256);
+
+
+type User = {
+    name: string;
+    email: string
+;
+}
+let MyUser:User[]=[];
+MyUser.push({name: "", email: ""});
+
+let MlModels:number[][] =[
+    [455,455],
+    [24]
+]
